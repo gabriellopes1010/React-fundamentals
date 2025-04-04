0cbf99bc10f908fd09775ba73e152c1150a7611e
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-04-04 12:04:51 -0300

    feat: :sparkles: adicionando o react fragment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+function App(){
+  return (
+    <>
+      <h1>Component App</h1>
+      <h1>Subtítulo</h1>
+    </>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Atualizado para usar createRoot
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement); // Cria a raiz do React
-
-function App(){
-  return <h1>Component App!</h1>
-}
 
 root.render(
   <App />
