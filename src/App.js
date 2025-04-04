4038d1ece48aa8384dc01a6937bf585c55bac847
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-04-04 13:08:34 -0300

    conhecendo as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Post from './Post';
+//Render -> Renderizars
 
 function App(){
   return (
     <>
-      <h1>Component App</h1>
-      <h1>Subtítulo</h1>
+      <h1>JStack's Blog</h1>
+      <h1>Posts da semana</h1>
+      <hr />
+
+      <Post
+        title="Titulo da notícia 01" 
+        subtitle ="subtitulo da notícia 01"
+      />
+      <Post
+        title="Titulo da notícia 02" 
+        subtitle ="subtitulo da notícia 02"
+      />
+      <Post
+        title="Titulo da notícia 03" 
+        subtitle ="subtitulo da notícia 03"
+      />
+      <Post
+        title="Titulo da notícia 04" 
+        subtitle ="subtitulo da notícia 04"
+      />
     </>
   )
 }
