a2a351cba729a0e2f38cc7ca7f8b0c4bfd6c7ad7
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-04-17 18:06:20 -0300

    adicionando a class

--- src/App.js
+++ src/App.js
@@ -6,22 +6,51 @@ import Layout from "./components/Layout";
 
 import themes from "./styles/themes";
 
-function App() {
-  const [theme, setTheme] = useState("dark");
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark;
-  }, [theme]);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  function handleToggleTheme() {
-    setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
+    this.state = {
+      theme: "dark",
+    };
+
+    this.handleToggleTheme = this.handleToggleTheme.bind(this);
   }
 
-  return (
-    <ThemeProvider theme={currentTheme}>
-      <GlobalStyle />
-      <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
-    </ThemeProvider>
-  );
+  handleToggleTheme() {
+    this.setState((prevState) => ({
+      theme: prevState.theme === "dark" ? "light" : "dark",
+    }));
+  }
+
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <ThemeProvider theme={themes[theme] || themes.dark}>
+        <GlobalStyle />
+        <Layout onToggleTheme={this.handleToggleTheme} selectedTheme={theme} />
+      </ThemeProvider>
+    );
+  }
 }
+
+// function App() {
+//   const [theme, setTheme] = useState("dark");
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
+
+//   function handleToggleTheme() {
+//     setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
+//   }
+
+//   return (
+//     <ThemeProvider theme={currentTheme}>
+//       <GlobalStyle />
+//       <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
+//     </ThemeProvider>
+//   );
+// }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,26 @@ import React from "react";
 
 import { Container } from "./styles";
 
-export default function Header({ onToggleTheme, selectedTheme }) {
-  return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button type="button" onClick={onToggleTheme}>
-        {selectedTheme === "dark" ? "🌞" : "🌙"}
-      </button>
-    </Container>
-  );
+export default class Header extends React.Component {
+  render() {
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button type="button" onClick={this.props.onToggleTheme}>
+          {this.props.selectedTheme === "dark" ? "🌞" : "🌙"}
+        </button>
+      </Container>
+    );
+  }
 }
+
+// export default function Header({ onToggleTheme, selectedTheme }) {
+//   return (
+//     <Container>
+//       <h1>JStack's Blog</h1>
+//       <button type="button" onClick={onToggleTheme}>
+//         {selectedTheme === "dark" ? "🌞" : "🌙"}
+//       </button>
+//     </Container>
+//   );
+// }
