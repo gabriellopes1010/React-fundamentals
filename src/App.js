a94a39ab02bfc05431f704c6f2ebb0fa574689ff
Gabriel Siqueira <gabriellopes1010@gmail.com>
2025-04-17 13:23:28 -0300

    adicionando o theming and icons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useMemo } from "react";
+import { ThemeProvider } from "styled-components";
 
-import GlobalStyle from './styles/global';
-import Layout from './components/Layout';
+import GlobalStyle from "./styles/global";
+import Layout from "./components/Layout";
+
+import themes from "./styles/themes";
 
 function App() {
+  const [theme, setTheme] = React.useState("dark");
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme((prevState) => (prevState === "dark" ? "light" : "dark"));
+  }
+
   return (
-    <>
+    <ThemeProvider theme={currentTheme}>
       <GlobalStyle />
-      <Layout />
-    </>
+      <Layout onToggleTheme={handleToggleTheme} selectedTheme={theme} />
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
--- src/components/PostsList/Post/styles.js
+++ src/components/PostsList/Post/styles.js
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.article`
-  background: #333;
+  background: ${(props) => props.theme.postBackgroundColor};
   display: flex;
   flex-direction: column;
   padding: 16px;
